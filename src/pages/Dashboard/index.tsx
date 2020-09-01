b7cac9a91614236dee765b7cc3ef4bf8802a61ef
Wylgner Emiliano <37162794+wylgneremiliano@users.noreply.github.com>
2020-09-01 12:57:17 -0300

    confiuração inicial + global css

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Title } from './styles'
+const Dashboard: React.FC = () => {
+    return <Title>Explore Repositórios no Github</Title>
+}
+
+export default Dashboard
